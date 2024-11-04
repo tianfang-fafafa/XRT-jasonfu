b097f0ea33ded537e423afb55592a356550d2f7c
Jason Fu <jason.fu@amd.com>
2024-11-04 03:48:51 -0600

    Debug: transaction timestamp sync v3

--- src/runtime_src/xdp/profile/plugin/ml_timeline/clientDev/ml_timeline.cpp
+++ src/runtime_src/xdp/profile/plugin/ml_timeline/clientDev/ml_timeline.cpp
@@ -24,6 +24,7 @@
 #include <regex>
 
 #include "core/common/api/bo_int.h"
+#include "core/common/api/hw_context_int.h"
 #include "core/common/device.h"
 #include "core/common/message.h"
 
@@ -32,6 +33,12 @@
 
 #include "xdp/profile/plugin/ml_timeline/clientDev/ml_timeline.h"
 #include "xdp/profile/plugin/vp_base/utility.h"
+#include "xdp/profile/device/common/client_transaction.h"
+
+extern "C" {
+#include <xaiengine.h>
+#include <xaiengine/xaiegbl_params.h>
+}
 
 namespace xdp {
 
@@ -90,6 +97,18 @@ namespace xdp {
     }
     xrt_core::message::send(xrt_core::message::severity_level::debug, "XRT", 
               "Allocated buffer In MLTimelineClientDevImpl::updateDevice");
+    if (xrt_core::config::get_verbosity() >= static_cast<uint32_t>(xrt_core::message::severity_level::debug)) {
+      mResultBOHolder->syncFromDevice();
+      memset(mResultBOHolder->map(), 0, mBufSz);
+      std::string msec_before = xdp::getMsecSinceEpoch();
+      uint64_t ts = readTimestamp();
+      std::string msec_after = xdp::getMsecSinceEpoch();
+      std::stringstream ssmsg;
+      ssmsg << "MsecSinceEpoch: " << msec_before
+          << ", Timestamp: " << ts << "(0x" << std::hex << ts << ")"
+          << ", MsecSinceEpoch: " << msec_after << std::endl;
+      xrt_core::message::send(xrt_core::message::severity_level::debug, "XRT", ssmsg.str());
+    }
   }
 
   void MLTimelineClientDevImpl::finishflushDevice(void* /*hwCtxImpl*/)
@@ -175,11 +194,95 @@ namespace xdp {
     xrt_core::message::send(xrt_core::message::severity_level::debug, "XRT", 
               "Finished writing record_timer_ts.json in MLTimelineClientDevImpl::finishflushDevice");
 
+    if (xrt_core::config::get_verbosity() >= static_cast<uint32_t>(xrt_core::message::severity_level::debug)) {
+      mResultBOHolder->syncFromDevice();
+      memset(mResultBOHolder->map(), 0, mBufSz);
+      std::string msec_before = xdp::getMsecSinceEpoch();
+      uint64_t ts = readTimestamp();
+      std::string msec_after = xdp::getMsecSinceEpoch();
+      std::stringstream ssmsg;
+      ssmsg << "MsecSinceEpoch: " << msec_before
+          << ", Timestamp: " << ts << "(0x" << std::hex << ts << ")"
+          << ", MsecSinceEpoch: " << msec_after << std::endl;
+      xrt_core::message::send(xrt_core::message::severity_level::debug, "XRT", ssmsg.str());
+    }
     /* Delete the result BO so that AIE Profile/Debug Plugins, if enabled,
      * can use their own Debug BO to capture their data.
      */
     delete mResultBOHolder;
     mResultBOHolder = nullptr;
+  }
+
+  void MLTimelineClientDevImpl::sendTimestampReadingCmd()
+  {
+    std::shared_ptr<xrt_core::device> coreDevice = xrt_core::hw_context_int::get_core_device(mHwContext);
+    if (coreDevice == nullptr) {
+      xrt_core::message::send(xrt_core::message::severity_level::error,
+          "XRT", "Failed to get device when try to parsing metadata.");
+      return;
+    }
+    auto data = coreDevice->get_axlf_section(AIE_METADATA);
+    metadataReader = aie::readAIEMetadata(data.first, data.second, aie_meta);
+    if (!metadataReader) {
+      xrt_core::message::send(xrt_core::message::severity_level::error,
+          "XRT", "Error parsing metadata.");
+      return;
+    }
+    xdp::aie::driver_config meta_config = metadataReader->getDriverConfig();
+
+    XAie_Config cfg {
+      meta_config.hw_gen,
+      meta_config.base_address,
+      meta_config.column_shift,
+      meta_config.row_shift,
+      meta_config.num_rows,
+      meta_config.num_columns,
+      meta_config.shim_row,
+      meta_config.mem_row_start,
+      meta_config.mem_num_rows,
+      meta_config.aie_tile_row_start,
+      meta_config.aie_tile_num_rows,
+      {0} // PartProp
+    };
+    XAie_DevInst aieDevInst = {0};
+    auto RC = XAie_CfgInitialize(&aieDevInst, &cfg);
+    if (RC != XAIE_OK) {
+      xrt_core::message::send(xrt_core::message::severity_level::error,
+          "XRT", "AIE Driver Initialization Failed.");
+      return;
+    }
+    //Start recording the transaction
+    XAie_StartTransaction(&aieDevInst, XAIE_TRANSACTION_DISABLE_AUTO_FLUSH);
+    uint32_t pad = 0;
+    XAie_AddCustomTxnOp(&aieDevInst, XAIE_IO_CUSTOM_OP_RECORD_TIMER, (void*)&pad, 4);
+    uint8_t *txn_ptr = XAie_ExportSerializedTransaction(&aieDevInst, 1, 0);
+    std::unique_ptr<aie::ClientTransaction> transactionHandler;
+    transactionHandler = std::make_unique<aie::ClientTransaction>(mHwContext, "AIE Profile Setup");
+    if (!transactionHandler->initializeKernel("XDP_KERNEL")) {
+      xrt_core::message::send(xrt_core::message::severity_level::error,
+          "XRT", "initializeKernel(\"XDP_KERNEL\") Failed.");
+      return;
+    }
+    if (!transactionHandler->submitTransaction(txn_ptr)) {
+      xrt_core::message::send(xrt_core::message::severity_level::error,
+          "XRT", "submitTransaction(txn_ptr) Failed.");
+      return;
+    }
+    // Must clear aie state
+    XAie_ClearTransaction(&aieDevInst);
+  }
+
+  std::vector<uint64_t> MLTimelineClientDevImpl::getTimestampReadingResult(uint32_t ts_num)
+  {
+    mResultBOHolder->syncFromDevice();
+    std::vector<uint64_t> result;
+    uint32_t* output = mResultBOHolder->map();
+    for (uint32_t i = 0; i < ts_num; i++, output += 3) {
+      uint64_t ts64 = *(output + 1);
+      ts64 = (ts64 << 32) | *(output + 2);
+      result.push_back(ts64);
+    }
+    return result;
   }
 }
 
